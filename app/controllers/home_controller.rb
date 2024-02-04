514fd27adcdee8871793e9b1c61c9ef60789c984
Ugo Asonye <ugonnaasonye@yahoo.com>
2024-02-04 14:06:44 -0600

    Name error uninitialized constant HomeController::API_key when using API_key in call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 #frozen_string_literal: true
+
+require 'net/http'
 class HomeController < ApplicationController
     def index
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=41.881832&lon=-87.623177&units=imperial@appid=#{API_key}"
+        uri = URI(url)
+        res = Net::HTTP.get_response(uri)
+        data = JSON.parse(res.body)
     end
 end
